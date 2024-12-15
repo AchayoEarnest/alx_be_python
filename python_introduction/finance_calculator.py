a9955e9128b3566557e9b8102f9dd57ef217767e
Earnest Achayo <earnytechlive@gmail.com>
2024-12-15 19:41:39 +0300

    bufixes

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-monthly_income = int(input("Enter your monthly income: "))
-monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
+# Calculate monthly savings
 monthly_savings = monthly_income - monthly_expenses
 
 annual_savings = monthly_savings * 12
 projected_annual_savings = annual_savings + (annual_savings * 0.05)
 
+print(f"Your monthly savings are ${monthly_savings:.2f}")
+print(f"Projected savings after one year, with interest, is: ${projected_annual_savings:.2f}")
 
-print("Projected savings after one year, with interest, is: ",projected_annual_savings)
