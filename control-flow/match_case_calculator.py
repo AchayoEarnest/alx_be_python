f0ad6194dc9ce4c182ff2b296b66443963c6bea0
Earnest Achayo <earnytechlive@gmail.com>
2024-12-23 00:26:06 +0300

    calculator added

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Prompt the user for input
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operator = input("Choose the operation (+, -, *, /): ").strip()
+
+# Match-case for operations
+match operator:
+    case "+":
+        result = num1 + num2
+        print(f"The result is {result}")
+    case "-":
+        result = num1 - num2
+        print(f"The result is {result}")
+    case "*":
+        result = num1 * num2
+        print(f"The result is {result}")
+    case "/":
+        if num2 == 0:
+            print("You cannot divide a number by 0.")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}")
+    case _:
+        print("Invalid input. Please choose a valid operation (+, -, *, /).")
