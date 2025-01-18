2bae3b9a8cc12ab9a00c53c83d2c2c693545fd7c
Earnest Achayo <earnytechlive@gmail.com>
2025-01-18 19:07:48 +0300

    created bank account app

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+class BankAccount:
+    def __init__(self, initial_balance: 0):
+        """Initializing account with default optonal balance"""
+        self.__account_balance = initial_balance
+        
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+    
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            return True
+        return False
+    
+    def display_balance(self):
+        """Print the current account balance"""
+        print(f"The current balance is {self.__account_balance}")
