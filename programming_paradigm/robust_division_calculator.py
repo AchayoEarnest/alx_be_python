84a6307c8755138e88f7931374167291e0e3a2b8
Earnest Achayo <earnytechlive@gmail.com>
2025-01-18 19:40:52 +0300

    robust division calculator added

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def safe_divide(numerator, denominator):
+    try:
+        num = float(numerator)
+        den = float(denominator)
+
+        if den == 0:
+            return "Error: Cannot divide by zero."
+        return f"The result of the division is {num/den}" 
+
+    except ValueError:
+        print("Error: Please enter numeric values only.")
+
